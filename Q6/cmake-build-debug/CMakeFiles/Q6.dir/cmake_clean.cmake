73a8bf5694961389c1fa80025ea39a81bde2ffe1
Kasra <kasrakhalafi@gmail.com>
2019-05-08 01:46:36 +0430

    HW4 added

---
+++ Q6/cmake-build-debug/CMakeFiles/Q6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q6.dir/main.cpp.o"
+  "Q6.pdb"
+  "Q6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
